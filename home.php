74d81a2d71cc601149be6a05f430449b802a11b7
muhammadilyas19 <mi374824@gmail.com>
2020-05-29 12:55:28 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,8 @@
+<p></p>
+<body>
+    <center><font size="+4" face="comic sans ms">Sistem Informasi Perpustakaan</font></center>
+    <center><font size="+4" face="comic sans ms">Sekolah Dasar Swasta Insani</font></center><br><br><br><br>
+
+    <img class="gambar" src="https://www.pngkey.com/png/full/340-3404484_1523344401-pen-il-logo-buku-vector-png.png" height="300px" width="500px" style="display: block; margin: auto"; >
+
+</body>
